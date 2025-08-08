e369c59bcffbef8c33b2315c3c628e767da03942
Marcelo Vieira <vieiramarcelo.vds@gmail.com>
2025-08-08 17:06:20 -0300

    adicionando opção das imagens trocarem automaticamente

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 "use strict"
 
 const botaoTrocarImagem = document.getElementById('trocar-imagem')
+const botaoAutomatico = document.getElementById('automatico')
 
 function trocarImagem(){
     const imagem = document.getElementById('imagem').value
     document.documentElement.style.setProperty('--bg-image', `url(./img/${imagem}.jpg)`)
 }
 
+function automatico() {
+    let contador = 1
+
+    setInterval(function() {
+
+        document.documentElement.style.setProperty('--bg-image', `url(./img/${contador}.jpg)`)
+        contador++
+        console.log(contador)
+
+        // quando passar da última imagem voltar para a primeira
+        if (contador > 7){
+            contador = 1
+        }
+
+    }, 1700) // troca a cada 1.7 segundos
+}
+
 botaoTrocarImagem.addEventListener('click', trocarImagem)
+botaoAutomatico.addEventListener('click', automatico)
